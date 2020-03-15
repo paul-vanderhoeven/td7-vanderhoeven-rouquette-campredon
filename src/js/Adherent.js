7b79efab5de4094e1f47db7035a0d59ddf493fc9
Paul van der Hoeven <paul.vdh@hotmail.fr>
2020-03-15 10:53:15 +0100

    ajout de sauvegarder dans adherent

--- src/js/Adherent.js
+++ src/js/Adherent.js
@@ -7,8 +7,17 @@ class Adherent {
 
   ajouterListe() {
     let li = document.createElement("li");
-    li.innerHTML = this.nomAdherent;
+    li.innerHTML = idAdherent + " - " + this.nomAdherent;
     document.getElementById("listeAdherents").appendChild(li);
+  }
+
+  sauvegarder() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "php/save.php?table=adherent&nomAdherent=" + this.nomAdherent, true);
+    xhr.send(null);
+
+    let xhr2 = new XMLHttpRequest();
+    xhr.open("GET", "php/select.php?table=adherent&nomAdherent=" + this.nomAdherent, true)
   }
 
   details() {
@@ -24,3 +33,6 @@ class Adherent {
     // TO DO
   }
 }
+
+let a = new Adherent(null, "Polo");
+a.sauvegarder();
